c067f5a68565258d5fa9e3c3291516af5c0f4a7f
Anton Konovalov <antoniuskonovalov@gmail.com>
2024-03-25 15:23:29 +0200

    buttons style

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,44 @@
+import { useState } from "react";
+import { motion, useCycle } from "framer-motion";
 import "./hero.scss";
 
+const variants = {
+  open: {
+    transition: {
+      type: "spring",
+      stiffness: 20,
+    },
+  },
+  closed: {
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
+
+const buttons = [
+  {
+    text: "See the Latest Work",
+  },
+  {
+    text: "Contact Me",
+  },
+];
+
 const Hero = () => {
+  const [activeBtn, setActiveBtn] = useState(Array(buttons.length).fill(false));
+  const [animate, cycleAnimate] = useCycle("closed", "open");
+
+  const toggleActive = (index) => {
+    setActiveBtn((prevStates) =>
+      prevStates.map((state, i) => (i === index ? !state : state))
+    );
+    cycleAnimate();
+  };
+
   return (
     <div className="hero">
       <div className="wrapper">
@@ -13,8 +51,19 @@ const Hero = () => {
           </div>
           <div className="btnImgContainer">
             <div className="buttons">
-              <button>See the Latest Work</button>
-              <button>Contact Me </button>
+              {/* <button>See the Latest Work</button>
+              <button>Contact Me </button> */}
+              {buttons.map((item, index) => (
+                <motion.button
+                  key={item.text}
+                  onClick={() => toggleActive(index)}
+                  className={activeBtn[index] ? "btnActive" : "btnNotActive"}
+                  whileTap={{ scale: 0.95 }}
+                  variants={variants}
+                >
+                  {item.text}
+                </motion.button>
+              ))}
             </div>
             <img src="/scroll.png" alt="" />
           </div>
